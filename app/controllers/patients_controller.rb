85d8a7c700311e2380c5c9f4e6156ae47cd264f5
William Feron <william_feron@hotmail.fr>
2023-09-28 17:26:01 +0200

    crud

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,37 @@
 class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def new
+    @restaurant = Patient.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    @patient.save
+    # No need for app/views/restaurants/create.html.erb
+    #redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    @patient.destroy
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:nom, :prenom, :telephone, :rue, :numero, :ville, :niss)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'page#home'
   get 'patients', to: 'patients#index'
+  get 'patients/new', to: 'patients#new'
+
   # Defines the root path route ("/")
   # root "articles#index"
   resources :patients
